54de785609ad6af0a0017b550457fba5fd9542aa
Jean-François de Locht <jf.delocht@gmail.com>
2013-08-11 18:07:51 +0200

    Add Compiler

---
+++ DependencyInjection/Compiler/FormPass.php
@@ -0,0 +1,27 @@
+<?php
+namespace Jfdl\Bundle\FormBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Add a new twig.form.resources
+ *
+ * @author Olivier Chauvel <olivier@generation-multiple.com>
+ */
+class FormPass implements CompilerPassInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $resources = $container->getParameter('twig.form.resources');
+
+        $resources[] = 'JfdlFormBundle:Form:div_layout.html.twig';
+
+        $container->setParameter('twig.form.resources', $resources);
+    }
+}
+
+?>
--- JfdlFormBundle.php
+++ JfdlFormBundle.php
@@ -3,7 +3,16 @@
 namespace Jfdl\Bundle\FormBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Jfdl\Bundle\FormBundle\DependencyInjection\Compiler\FormPass;
 
 class JfdlFormBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new FormPass());
+    }
 }
